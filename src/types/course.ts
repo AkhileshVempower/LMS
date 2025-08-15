d9c686387a7265e6f3cf02a8a256944b79148fb5
Akhilesh Pratap <akhilesh2060@gmail.com>
2025-08-15 20:57:42 +0530

    Add course offering type

---
+++ src/types/course.ts
@@ -0,0 +1,9 @@
+export interface CourseOffering {
+  course_id: string;
+  course_name: string;
+  instructor_name: string;
+  start_date: string;
+  min_employees: number;
+  max_employees: number;
+  registered_count: number;
+}
